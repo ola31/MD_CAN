4bdbba7aa05109c1d2910564dfaa544156f7ecdf
ola <abrahamola80@gmail.com>
2020-12-20 00:44:52 +0900

    터틀봇 코드 추가(에러뜸)

--- MD_CAN.ino
+++ MD_CAN.ino
@@ -8,13 +8,6 @@
 #include <std_msgs/UInt8MultiArray.h>
 #include <std_msgs/Int32.h>
 
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/JointState.h>
-#include <geometry_msgs/Vector3.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-
 
 #include <CAN.h>
 #include<stdio.h>                
@@ -28,7 +21,170 @@
 #define wheel_radius 0.08  //바퀴 반지름
 #define gear_ratio 1       //원래 기어비 : 30
 
+/********************************************************************
+*터틀봇코드관련 include (turtlebot3_burger.h)
+********************************************************************/
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/BatteryState.h>
+#include <sensor_msgs/MagneticField.h>
+#include <geometry_msgs/Vector3.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <math.h>
+/********************************************************************
+*터틀봇코드관련 define (turtlebot3_burger.h)
+********************************************************************/
+#define WHEEL_RADIUS                     0.033           // meter
+#define WHEEL_SEPARATION                 0.160           // meter (BURGER : 0.160, WAFFLE : 0.287)
+#define TURNING_RADIUS                   0.080           // meter (BURGER : 0.080, WAFFLE : 0.1435)
+#define ROBOT_RADIUS                     0.105           // meter (BURGER : 0.105, WAFFLE : 0.220)
+#define ENCODER_MIN                      -2147483648     // raw
+#define ENCODER_MAX                      2147483648      // raw
+
+#define MAX_LINEAR_VELOCITY              (WHEEL_RADIUS * 2 * 3.14159265359 * 61 / 60) // m/s  (BURGER : 61[rpm], WAFFLE : 77[rpm])
+#define MAX_ANGULAR_VELOCITY             (MAX_LINEAR_VELOCITY / TURNING_RADIUS)       // rad/s
+
+#define MIN_LINEAR_VELOCITY              -MAX_LINEAR_VELOCITY  
+#define MIN_ANGULAR_VELOCITY             -MAX_ANGULAR_VELOCITY 
+
+/********************************************************************
+*터틀봇코드관련 define (turtlebot3_core_config.h)
+********************************************************************/
+#define FIRMWARE_VER "1.2.3"
+
+#define CONTROL_MOTOR_SPEED_FREQUENCY          30   //hz
+#define CONTROL_MOTOR_TIMEOUT                  500  //ms
+#define IMU_PUBLISH_FREQUENCY                  200  //hz
+#define CMD_VEL_PUBLISH_FREQUENCY              30   //hz
+#define DRIVE_INFORMATION_PUBLISH_FREQUENCY    30   //hz
+#define VERSION_INFORMATION_PUBLISH_FREQUENCY  1    //hz 
+#define DEBUG_LOG_FREQUENCY                    10   //hz 
+
+#define WHEEL_NUM                        2
+
+#define LEFT                             0
+#define RIGHT                            1
+
+#define LINEAR                           0
+#define ANGULAR                          1
+
+#define DEG2RAD(x)                       (x * 0.01745329252)  // *PI/180
+#define RAD2DEG(x)                       (x * 57.2957795131)  // *180/PI
+
+#define TICK2RAD                         0.001533981  // 0.087890625[deg] * 3.14159265359 / 180 = 0.001533981f
+
+#define TEST_DISTANCE                    0.300     // meter
+#define TEST_RADIAN                      3.14      // 180 degree
+
+
+/*******************************************************************************
+ * 터틀봇코드 관련 함수정의(turtlebot3_core_config.h)
+ ******************************************************************************/
+
+// Function prototypes
+
+void publishImuMsg(void);
+void publishSensorStateMsg(void);
+void publishVersionInfoMsg(void);
+void publishBatteryStateMsg(void);
+void publishDriveInformation(void);
+
+ros::Time rosNow(void);
+ros::Time addMicros(ros::Time & t, uint32_t _micros); // deprecated
+
+void updateVariable(bool isConnected);
+void updateMotorInfo(int32_t left_tick, int32_t right_tick);
+void updateTime(void);
+void updateOdometry(void);
+void updateJoint(void);
+void updateTF(geometry_msgs::TransformStamped& odom_tf);
+void updateGyroCali(bool isConnected);
+void updateGoalVelocity(void);
+void updateTFPrefix(bool isConnected);
+
+void initOdom(void);
+void initJointStates(void);
+
+bool calcOdometry(double diff_time);
+
+void waitForSerialLink(bool isConnected);
+
+/*******************************************************************************
+* ROS NodeHandle
+*******************************************************************************/
+//ros::NodeHandle nh;
+ros::Time current_time;
+uint32_t current_offset;
+
+/*******************************************************************************
+* ROS Parameter
+*******************************************************************************/
+char get_prefix[10];
+char* get_tf_prefix = get_prefix;
+
+char odom_header_frame_id[30];
+char odom_child_frame_id[30];
+
+char imu_frame_id[30];
+char mag_frame_id[30];
+
+char joint_state_header_frame_id[30];
+
+/*******************************************************************************
+* Publisher
+*******************************************************************************/
+// Bumpers, cliffs, buttons, encoders, battery of Turtlebot3
+turtlebot3_msgs::SensorState sensor_state_msg;
+ros::Publisher sensor_state_pub("sensor_state", &sensor_state_msg);
+
+// Version information of Turtlebot3
+turtlebot3_msgs::VersionInfo version_info_msg;
+ros::Publisher version_info_pub("firmware_version", &version_info_msg);
+
+// IMU of Turtlebot3
+sensor_msgs::Imu imu_msg;
+ros::Publisher imu_pub("imu", &imu_msg);
+
+// Command velocity of Turtlebot3 using RC100 remote controller
+geometry_msgs::Twist cmd_vel_rc100_msg;
+ros::Publisher cmd_vel_rc100_pub("cmd_vel_rc100", &cmd_vel_rc100_msg);
+
+// Odometry of Turtlebot3
+nav_msgs::Odometry odom;
+ros::Publisher odom_pub("odom", &odom);
+
+// Joint(Dynamixel) state of Turtlebot3
+sensor_msgs::JointState joint_states;
+ros::Publisher joint_states_pub("joint_states", &joint_states);
+
+// Battey state of Turtlebot3
+sensor_msgs::BatteryState battery_state_msg;
+ros::Publisher battery_state_pub("battery_state", &battery_state_msg);
+
+// Magnetic field
+sensor_msgs::MagneticField mag_msg;
+ros::Publisher mag_pub("magnetic_field", &mag_msg);
+
+/*******************************************************************************
+* Transform Broadcaster
+*******************************************************************************/
+// TF of Turtlebot3
+geometry_msgs::TransformStamped odom_tf;
+tf::TransformBroadcaster tf_broadcaster;
+
+
+
+
+
+ 
+/*******************************************************************************
+ * 기존 함수,변수선언(CAN통신 등 관련)
+ ******************************************************************************/
 
 static int16_t R_RPM=0,L_RPM=0;  //  -32768<={ RPM }<=32767  (RPM범위)(32767보다 크면 음수화, -32768보다 작으면 오버플로우)
 
@@ -59,10 +215,6 @@ ros::Publisher pub2("l_rpm", &msg2); //추가
 
 std_msgs::UInt8MultiArray re_msg;
 ros::Publisher pub("recived", &re_msg);  //CAN리턴값 pub
-
-
-
-
 
 
 
@@ -194,7 +346,7 @@ can_message_t tx_msg, rx_msg;
 
 // 쓰레드관련
 osThreadId thread_id_CAN;
-osThreadId thread_id_COMMAND;
+osThreadId thread_id_ROS;
 
 
 
@@ -244,22 +396,31 @@ static void Thread_CAN(void const *argument)
 
 
 /************************************************************
-*     <THREAD_NAME_COMMAND 스레드에서 돌아가는 함수> 
-*       ROS spinonce 가 스레드에서 계속 돌아가게 함 
+*     <THREAD_NAME_ROS 스레드에서 돌아가는 함수> 
+*       ROS spinonce가 스레드에서 계속 돌아가게 함 
 ************************************************************/
 
-static void Thread_Command(void const *argument)
+static void Thread_ROS(void const *argument)
 {
   (void) argument;
 
   for(;;)
-  { 
-    if(CanBus.readMessage(&rx_msg))
-    {
-        for(i=0;i<8;i++){CAN_recieved[i]=rx_msg.data[i];}     
+  {
+    //터틀봇코드
+    uint32_t t = millis();
+    updateTime();
+    updateVariable(nh.connected());
+    updateTFPrefix(nh.connected()); 
+
+    //원래코드(CAN 회신데이터 publish)
+    if(CanBus.readMessage(&rx_msg)){
+        
+        for(i=0;i<8;i++)
+        {
+          CAN_recieved[i]=rx_msg.data[i];
+        }     
     
         re_msg.data[0]=CAN_recieved[0];
-        
         
         re_msg.data = CAN_recieved;
         re_msg.data_length = 8;
@@ -271,6 +432,10 @@ static void Thread_Command(void const *argument)
     msg2.data=(int)(L_RPM); //추가
     pub1.publish(&msg1); //추가
     pub2.publish(&msg2);
+
+    //터틀봇코드
+    
+    
     
     nh.spinOnce();
     delay(5);   //can통신을 보내주는 delay보다 짧아야 한다. 
@@ -336,11 +501,11 @@ void setup()
 
   // define thread
   osThreadDef(THREAD_NAME_CAN, Thread_CAN, osPriorityNormal, 0, 1024);
-  osThreadDef(THREAD_NAME_COMMAND,  Thread_Command,  osPriorityNormal, 0, 1024);
+  osThreadDef(THREAD_NAME_ROS, Thread_ROS, osPriorityNormal, 0, 1024);
  
   // create thread
   thread_id_CAN = osThreadCreate(osThread(THREAD_NAME_CAN), NULL);
-  thread_id_COMMAND = osThreadCreate(osThread(THREAD_NAME_COMMAND), NULL);
+  thread_id_ROS = osThreadCreate(osThread(THREAD_NAME_ROS), NULL);
 
 
   // start kernel
