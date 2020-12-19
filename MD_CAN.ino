a72b9193bc26934f075ca1e2fcc949cdb010d71a
ola <abrahamola80@gmail.com>
2020-12-19 17:12:25 +0900

    devel commit

--- MD_CAN.ino
+++ MD_CAN.ino
@@ -8,6 +8,14 @@
 #include <std_msgs/UInt8MultiArray.h>
 #include <std_msgs/Int32.h>
 
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Vector3.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+
 #include <CAN.h>
 #include<stdio.h>                
 #include<stdlib.h> 
@@ -20,11 +28,6 @@
 #define wheel_radius 0.08  //바퀴 반지름
 #define gear_ratio 1       //원래 기어비 : 30
 
-
-std_msgs::Int32 msg1;  //추가
-std_msgs::Int32 msg2;  //추가
-ros::Publisher pub1("r_rpm", &msg1); //추가
-ros::Publisher pub2("l_rpm", &msg2); //추가
 
 
 static int16_t R_RPM=0,L_RPM=0;  //  -32768<={ RPM }<=32767  (RPM범위)(32767보다 크면 음수화, -32768보다 작으면 오버플로우)
@@ -46,6 +49,12 @@ ros::NodeHandle nh;
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", VelCb );             //선,각속도 sub
 ros::Subscriber<std_msgs::UInt8> sub2("cmd_mode", Switch_command );       //배열번호값 sub
 ros::Subscriber<std_msgs::UInt8MultiArray> sub3("mdcan", mdcanCallback);  //직접입력한 CAN data값 sub
+
+
+std_msgs::Int32 msg1;  //추가
+std_msgs::Int32 msg2;  //추가
+ros::Publisher pub1("r_rpm", &msg1); //추가
+ros::Publisher pub2("l_rpm", &msg2); //추가
 
 
 std_msgs::UInt8MultiArray re_msg;
@@ -225,7 +234,7 @@ static void Thread_CAN(void const *argument)
       {
           CAN_Until_8(goArr);
       }
-      delay(500);
+      delay(30);
     
   }
   
@@ -294,6 +303,24 @@ void setup()
   nh.subscribe(sub3);
 
   //nh.advertise(pub);//추가
+
+  nh.advertise(sensor_state_pub);  
+  nh.advertise(version_info_pub);
+  nh.advertise(imu_pub);
+  nh.advertise(cmd_vel_rc100_pub);
+  nh.advertise(odom_pub);
+  nh.advertise(joint_states_pub);
+  nh.advertise(battery_state_pub);
+  nh.advertise(mag_pub);
+
+  tf_broadcaster.init(nh);
+
+  // Setting for SLAM and navigation (odometry, joint states, TF)
+  initOdom();
+
+  initJointStates();
+
+
   
   Serial.begin(115200);
   
@@ -389,4 +416,336 @@ void cmd_vel2RPM(float linear_vel, float angular_vel)
   
 }
 
+
+
+
+
+
+
+
+
+/*********************************************************************************/
+
+/*******************************************************************************
+* Callback function for reset msg
+*******************************************************************************/
+void resetCallback(const std_msgs::Empty& reset_msg)
+{ 
+  char log_msg[50];
+
+  (void)(reset_msg);
+
+  sprintf(log_msg, "Start Calibration of Gyro");
+  nh.loginfo(log_msg);
+
+  sensors.calibrationGyro();
+
+  sprintf(log_msg, "Calibration End");
+  nh.loginfo(log_msg);
+
+  initOdom();
+
+  sprintf(log_msg, "Reset Odometry");
+  nh.loginfo(log_msg);  
+}
+
+
+/*******************************************************************************
+* Publish msgs (IMU data: angular velocity, linear acceleration, orientation)
+*******************************************************************************/
+void publishImuMsg(void)
+{
+  imu_msg = sensors.getIMU();
+
+  imu_msg.header.stamp    = rosNow();
+  imu_msg.header.frame_id = imu_frame_id;
+
+  imu_pub.publish(&imu_msg);
+}
+
+
+/*******************************************************************************
+* Publish msgs (version info)
+*******************************************************************************/
+void publishVersionInfoMsg(void)
+{
+  version_info_msg.hardware = "0.0.0";
+  version_info_msg.software = "0.0.0";
+  version_info_msg.firmware = FIRMWARE_VER;
+
+  version_info_pub.publish(&version_info_msg);
+}
+
+/*******************************************************************************
+* Publish msgs (battery_state)
+*******************************************************************************/
+void publishBatteryStateMsg(void)
+{
+  battery_state_msg.header.stamp = rosNow();
+  battery_state_msg.design_capacity = 1.8f; //Ah
+  battery_state_msg.voltage = sensors.checkVoltage();
+  battery_state_msg.percentage = (float)(battery_state_msg.voltage / 11.1f);
+
+  if (battery_state == 0)
+    battery_state_msg.present = false;
+  else
+    battery_state_msg.present = true;  
+
+  battery_state_pub.publish(&battery_state_msg);
+}
+
+
+
+/*******************************************************************************
+* Publish msgs (odometry, joint states, tf)
+*******************************************************************************/
+void publishDriveInformation(void)
+{
+  unsigned long time_now = millis();
+  unsigned long step_time = time_now - prev_update_time;
+
+  prev_update_time = time_now;
+  ros::Time stamp_now = rosNow();
+
+  // calculate odometry
+  calcOdometry((double)(step_time * 0.001));
+
+  // odometry
+  updateOdometry();
+  odom.header.stamp = stamp_now;
+  odom_pub.publish(&odom);
+
+  // odometry tf
+  updateTF(odom_tf);
+  odom_tf.header.stamp = stamp_now;
+  tf_broadcaster.sendTransform(odom_tf);
+
+  // joint states
+  updateJointStates();
+  joint_states.header.stamp = stamp_now;
+  joint_states_pub.publish(&joint_states);
+}
+
+
+
+
+/*******************************************************************************
+* Update TF Prefix
+*******************************************************************************/
+void updateTFPrefix(bool isConnected)
+{
+  static bool isChecked = false;
+  char log_msg[50];
+
+  if (isConnected)
+  {
+    if (isChecked == false)
+    {
+      nh.getParam("~tf_prefix", &get_tf_prefix);
+
+      if (!strcmp(get_tf_prefix, ""))
+      {
+        sprintf(odom_header_frame_id, "odom");
+        sprintf(odom_child_frame_id, "base_footprint");  
+
+        sprintf(imu_frame_id, "imu_link");
+        sprintf(mag_frame_id, "mag_link");
+        sprintf(joint_state_header_frame_id, "base_link");
+      }
+      else
+      {
+        strcpy(odom_header_frame_id, get_tf_prefix);
+        strcpy(odom_child_frame_id, get_tf_prefix);
+
+        strcpy(imu_frame_id, get_tf_prefix);
+        strcpy(mag_frame_id, get_tf_prefix);
+        strcpy(joint_state_header_frame_id, get_tf_prefix);
+
+        strcat(odom_header_frame_id, "/odom");
+        strcat(odom_child_frame_id, "/base_footprint");
+
+        strcat(imu_frame_id, "/imu_link");
+        strcat(mag_frame_id, "/mag_link");
+        strcat(joint_state_header_frame_id, "/base_link");
+      }
+
+      sprintf(log_msg, "Setup TF on Odometry [%s]", odom_header_frame_id);
+      nh.loginfo(log_msg); 
+
+      sprintf(log_msg, "Setup TF on IMU [%s]", imu_frame_id);
+      nh.loginfo(log_msg); 
+
+      sprintf(log_msg, "Setup TF on MagneticField [%s]", mag_frame_id);
+      nh.loginfo(log_msg); 
+
+      sprintf(log_msg, "Setup TF on JointState [%s]", joint_state_header_frame_id);
+      nh.loginfo(log_msg); 
+
+      isChecked = true;
+    }
+  }
+  else
+  {
+    isChecked = false;
+  }
+}
+
+
+
+/*******************************************************************************
+* Update the odometry
+*******************************************************************************/
+void updateOdometry(void)
+{
+  odom.header.frame_id = odom_header_frame_id;
+  odom.child_frame_id  = odom_child_frame_id;
+
+  odom.pose.pose.position.x = odom_pose[0];
+  odom.pose.pose.position.y = odom_pose[1];
+  odom.pose.pose.position.z = 0;
+  odom.pose.pose.orientation = tf::createQuaternionFromYaw(odom_pose[2]);
+
+  odom.twist.twist.linear.x  = odom_vel[0];
+  odom.twist.twist.angular.z = odom_vel[2];
+}
+
+
+
+/*******************************************************************************
+* Update the joint states 
+*******************************************************************************/
+void updateJointStates(void)
+{
+  static float joint_states_pos[WHEEL_NUM] = {0.0, 0.0};
+  static float joint_states_vel[WHEEL_NUM] = {0.0, 0.0};
+  //static float joint_states_eff[WHEEL_NUM] = {0.0, 0.0};
+
+  joint_states_pos[LEFT]  = last_rad[LEFT];
+  joint_states_pos[RIGHT] = last_rad[RIGHT];
+
+  joint_states_vel[LEFT]  = last_velocity[LEFT];
+  joint_states_vel[RIGHT] = last_velocity[RIGHT];
+
+  joint_states.position = joint_states_pos;
+  joint_states.velocity = joint_states_vel;
+}
+
+
+
+
+/*******************************************************************************
+* Update variable (initialization)
+*******************************************************************************/
+void updateVariable(bool isConnected)
+{
+  static bool variable_flag = false;
+  
+  if (isConnected)
+  {
+    if (variable_flag == false)
+    {      
+      sensors.initIMU();
+      initOdom();
+
+      variable_flag = true;
+    }
+  }
+  else
+  {
+    variable_flag = false;
+  }
+}
+
+
+/*******************************************************************************
+* CalcUpdateulate the TF
+*******************************************************************************/
+void updateTF(geometry_msgs::TransformStamped& odom_tf)
+{
+  odom_tf.header = odom.header;
+  odom_tf.child_frame_id = odom.child_frame_id;
+  odom_tf.transform.translation.x = odom.pose.pose.position.x;
+  odom_tf.transform.translation.y = odom.pose.pose.position.y;
+  odom_tf.transform.translation.z = odom.pose.pose.position.z;
+  odom_tf.transform.rotation      = odom.pose.pose.orientation;
+}
+
+
+/*******************************************************************************
+* Initialization joint states data
+*******************************************************************************/
+void initJointStates(void)
+{
+  static char *joint_states_name[] = {(char*)"wheel_left_joint", (char*)"wheel_right_joint"};
+
+  joint_states.header.frame_id = joint_state_header_frame_id;
+  joint_states.name            = joint_states_name;
+
+  joint_states.name_length     = WHEEL_NUM;
+  joint_states.position_length = WHEEL_NUM;
+  joint_states.velocity_length = WHEEL_NUM;
+  joint_states.effort_length   = WHEEL_NUM;
+}
+
+
+
+
+/*******************************************************************************
+* Calculate the odometry
+*******************************************************************************/
+bool calcOdometry(double diff_time)
+{
+  float* orientation;
+  double wheel_l, wheel_r;      // rotation value of wheel [rad]
+  double delta_s, theta, delta_theta;
+  static double last_theta = 0.0;
+  double v, w;                  // v = translational velocity [m/s], w = rotational velocity [rad/s]
+  double step_time;
+
+  wheel_l = wheel_r = 0.0;
+  delta_s = delta_theta = theta = 0.0;
+  v = w = 0.0;
+  step_time = 0.0;
+
+  step_time = diff_time;
+
+  if (step_time == 0)
+    return false;
+
+  wheel_l = TICK2RAD * (double)last_diff_tick[LEFT];
+  wheel_r = TICK2RAD * (double)last_diff_tick[RIGHT];
+
+  if (isnan(wheel_l))
+    wheel_l = 0.0;
+
+  if (isnan(wheel_r))
+    wheel_r = 0.0;
+
+  delta_s     = WHEEL_RADIUS * (wheel_r + wheel_l) / 2.0;
+  // theta = WHEEL_RADIUS * (wheel_r - wheel_l) / WHEEL_SEPARATION;  
+  orientation = sensors.getOrientation();
+  theta       = atan2f(orientation[1]*orientation[2] + orientation[0]*orientation[3], 
+                0.5f - orientation[2]*orientation[2] - orientation[3]*orientation[3]);
+
+  delta_theta = theta - last_theta;
+
+  // compute odometric pose
+  odom_pose[0] += delta_s * cos(odom_pose[2] + (delta_theta / 2.0));
+  odom_pose[1] += delta_s * sin(odom_pose[2] + (delta_theta / 2.0));
+  odom_pose[2] += delta_theta;
+
+  // compute odometric instantaneouse velocity
+
+  v = delta_s / step_time;
+  w = delta_theta / step_time;
+
+  odom_vel[0] = v;
+  odom_vel[1] = 0.0;
+  odom_vel[2] = w;
+
+  last_velocity[LEFT]  = wheel_l / step_time;
+  last_velocity[RIGHT] = wheel_r / step_time;
+  last_theta = theta;
+
+  return true;
 }
